619e015622ec659a0e685f737913b0d70c2a40e6
Fbayons <ferranbayona@gmail.com>
2020-10-26 19:03:56 +0100

    Creacion de clases

---
+++ src/test/java/BattleShip/ShipTest.java
@@ -0,0 +1,14 @@
+package BattleShip;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShipTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
