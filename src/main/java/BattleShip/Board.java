0a962695b0a9fda2c303746bab8b13e6bc0fa48a
Fbayons <ferranbayona@gmail.com>
2020-11-07 20:41:09 +0100

    Test inicializar matriz

--- src/main/java/BattleShip/Board.java
+++ src/main/java/BattleShip/Board.java
@@ -1,6 +1,24 @@
 package BattleShip;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class Board {
 	public static int rows = 10;
 	public static int columns = 10;
+	public static int[][] board;
+	
+	public void initboard() {
+		
+		
+	}
+
+
+	public Object getPosition(int x, int y) {
+		
+		return board[x][y];
+		
+	}
+	
 }
--- src/test/java/BattleShip/BoardTest.java
+++ src/test/java/BattleShip/BoardTest.java
@@ -6,9 +6,20 @@ import org.junit.Test;
 
 public class BoardTest {
 
+	public static int rows = 10;
+	public static int columns = 10;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void initBoardTest() {
+		
+		Board board = new Board();
+		board.initboard();
+
+		for (int i = 0; i < rows ; i++) {
+			for (int j = 0; j < columns; j++) {
+				assertEquals(0,board.getPosition(i,j));
+			}
+		}
 	}
 
 }
