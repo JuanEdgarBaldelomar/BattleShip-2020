3a23db9aa12a5e715f580461f558209115357f91
Fbayons <ferranbayona@gmail.com>
2020-11-08 10:23:14 +0100

    Test posicion barcos

--- src/main/java/BattleShip/Board.java
+++ src/main/java/BattleShip/Board.java
@@ -1,28 +1,27 @@
 package BattleShip;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 
 public class Board {
 	public static int rows = 10;
 	public static int columns = 10;
-	public static int[][] board;
+	protected int[][] matrix;
 	
 	public void initBoard() {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				board[i][j] = 0;
+				matrix[i][j] = 0;
 			}
 		}
+		
+	}
+	
+	public void shipPosition() {
 		
 	}
 
 
 	public Object getPosition(int x, int y) {
-		
-		return board[x][y];
-		
+		return matrix[x][y];	
 	}
 	
 }
--- src/test/java/BattleShip/BoardTest.java
+++ src/test/java/BattleShip/BoardTest.java
@@ -21,5 +21,26 @@ public class BoardTest {
 			}
 		}
 	}
+	/*
+	 * Test para comprobar que los barcos estan dentro de la matriz.
+	 * La suma de las longitudes de los barcos es 14.
+	 * */
+	@Test
+	public void shipPositionTest() {
+		
+		Board board = new Board();
+		board.initBoard();
+		board.shipPosition();
+		int counter = 0;
+		
+		for (int i = 0; i < rows ; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (board.matrix[i][j] == '!') {
+					counter++;
+				}
+			}
+		}
+		assertEquals(14, counter);
+	}
 
 }
