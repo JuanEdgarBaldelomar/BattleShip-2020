f76b6cdafd150fd9c24ed03a4153d4a7f1d8a02d
darkneo98 <49984593+darkneo98@users.noreply.github.com>
2020-11-14 21:45:43 +0100

    Test Game

--- src/test/java/BattleShip/GameTest.java
+++ src/test/java/BattleShip/GameTest.java
@@ -8,7 +8,22 @@ public class GameTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Board board = new Board(1,1,1);
+		Shooter Player= new Shooter(1);
+		Shooter CPU=new Shooter(2);
+		boolean winPlayer=false;
+		boolean winCPU=false;
+		board.initBoard();
+		
+		//PLAYER GANA
+		winPlayer=Player.Shot(board,1);
+		assertEquals(true,winPlayer);
+		assertEquals(false,winCPU);
+		//PLAYER PIERDE
+		winCPU=CPU.Shot(board,2);
+		assertEquals(true,winCPU);
+		assertEquals(false,winPlayer);
+		
 	}
 
 }
