94a172011aa592563b4153cd88b8eacbbe8cf127
darkneo98 <49984593+darkneo98@users.noreply.github.com>
2020-11-14 20:48:13 +0100

    Test getCoordinates

--- src/main/java/BattleShip/Shooter.java
+++ src/main/java/BattleShip/Shooter.java
@@ -1,5 +1,18 @@
 package BattleShip;
 
 public class Shooter {
-
+	private int x;
+	private int y;
+	private int id;
+	
+	public void getCoordinates(Board board,int id) {
+		
+		/*Controla El turno del Player y CPU, obteniendo coordenadas
+		por teclado y random y actualiza el board*/
+		
+	}
+	public int getId() {
+		
+		return this.id;
+	}
 }
--- src/test/java/BattleShip/ShooterTest.java
+++ src/test/java/BattleShip/ShooterTest.java
@@ -1,5 +1,6 @@
 package BattleShip;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,39 @@ import org.junit.jupiter.api.Test;
 class ShooterTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getCoordinatesTest() {
+		int countO=0,countX=0,countS=0;
+		
+	
+		Board board = new Board(5,5,5);
+		board.initBoard();
+		//setShip
+		board.matrix[1][1]='S';
+		board.matrix[3][3]='S';
+		board.matrix[2][4]='S';
+		board.matrix[1][2]='S';
+		board.matrix[2][2]='S';
+		
+		//Turno
+		board.matrix[0][0] ='O';
+		board.matrix[3][4] ='O';
+		board.matrix[1][1] ='X';
+		board.matrix[2][2] ='X';
+		
+		for (int i = 0; i < board.getRow() ; i++) {
+			for (int j = 0; j < board.getCol(); j++) {
+				if(board.matrix[i][j]=='O') {
+					countO++;
+				}else if(board.matrix[i][j]=='X'){
+					countX++;
+				}else if(board.matrix[i][j]=='S'){
+					countS++;
+				}
+			}
+		}
+		assertEquals(2, countO);
+		assertEquals(2, countX);
+		assertEquals(3, countS);
 	}
 
 }
